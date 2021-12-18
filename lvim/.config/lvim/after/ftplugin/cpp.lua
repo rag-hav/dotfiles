3f0deb94a20974a00d85187e992a72db0cc19e3a
Raghav Agarwal <raghav.ag13@gmail.com>
2021-12-18 22:29:19 +0530

    changes

--- lvim/.config/lvim/after/ftplugin/cpp.lua
+++ lvim/.config/lvim/after/ftplugin/cpp.lua
@@ -1,16 +1,19 @@
+local template_cmd = "<CMD>lua require('toggleterm.terminal').Terminal:new({ cmd = '%s', direction = 'vertical', close_on_exit = false }):open(50)<CR>"
+
 require("which-key").register(
     {
-        r={
+        r = {
             name = "Run",
-            r={"<CMD>w | TermExec cmd=\"make %:r && ./%:r \" direction=vertical size=50 <CR>", "Run"},
-            i={"<CMD>w | TermExec cmd=\"make %:r && ./%:r < in \" direction=vertical size=50 <CR>", "Input file (in)"},
+            r = {string.format(template_cmd, "make " .. vim.fn.expand("%:r") .. " && ./" .. vim.fn.expand("%:r")), "Run"},
+            i = {string.format(template_cmd, "make " .. vim.fn.expand("%:r") .. " && ./" .. vim.fn.expand("%:r") .. " < in"), "Run with input"},
+            -- r={"<CMD>w | TermExec cmd=\"make %:r && ./%:r \" direction=vertical size=50 <CR>", "Run"},
+            -- i={"<CMD>w | TermExec cmd=\"make %:r && ./%:r < in \" direction=vertical size=50 <CR>", "Input file (in)"},
 
             -- these are setup specific functions
-            c={"<CMD>1TermExec cmd=\"clear\" open=0 <CR>", "Filetester"},
-            f={"<CMD>w | 1TermExec cmd=\"ftester %:r \" direction=vertical size=50 <CR>", "Filetester"},
-            t={"<CMD>w | 1TermExec cmd=\"tester %:r \" direction=vertical size=50 <CR>", "Filetester"},
-            q={"<CMD>w | 1TermExec cmd=\"qtester %:r \" direction=vertical size=50 <CR>", "Filetester"},
+            f = {string.format(template_cmd, "source ~/.funcs.sh; ftester " .. vim.fn.expand("%:r")), "Full tester"},
+            q = {string.format(template_cmd, "source ~/.funcs.sh; qtester " .. vim.fn.expand("%:r")), "Quiet tester"},
+            t = {string.format(template_cmd, "source ~/.funcs.sh; tester " .. vim.fn.expand("%:r")), "Tester"},
         }
     },
-    { prefix ='<leader>', buffer = 0})
+    { prefix = '<leader>', buffer = 0})
 
