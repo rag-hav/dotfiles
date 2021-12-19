efe380bdf220dea7e31d4af6e8a61eaf9096ea5a
Raghav Agarwal <raghav.ag13@gmail.com>
2021-12-20 00:48:22 +0530

    changes

--- lvim/.config/lvim/lua/cached_format.lua
+++ lvim/.config/lvim/lua/cached_format.lua
@@ -1,39 +1,38 @@
-local function select_client(method)
-    local clients = vim.tbl_values(vim.lsp.buf_get_clients());
-    clients = vim.tbl_filter(function (client)
-        return client.supports_method(method)
-    end, clients)
-    -- better UX when choices are always in the same order (between restarts)
-    table.sort(clients, function (a, b) return a.name < b.name end)
+local function select_client(method, on_choice)
+  vim.validate {
+    on_choice = { on_choice, 'function', false },
+  }
+  local clients = vim.tbl_values(vim.lsp.buf_get_clients())
+  clients = vim.tbl_filter(function(client)
+    return client.supports_method(method)
+  end, clients)
+  -- better UX when choices are always in the same order (between restarts)
+  table.sort(clients, function(a, b)
+    return a.name < b.name
+  end)
 
-    if #clients > 1 then
-        local choices = {}
-        for k,v in pairs(clients) do
-            table.insert(choices, string.format("%d %s", k, v.name))
-        end
-        local user_choice = vim.fn.confirm(
-        "Select a language server:",
-        table.concat(choices, "\n"),
-        0,
-        "Question"
-        )
-        if user_choice == 0 then return nil end
-        return clients[user_choice]
-    elseif #clients < 1 then
-        return nil
-    else
-        return clients[1]
-    end
+  if #clients > 1 then
+    vim.ui.select(clients, {
+      prompt = 'Select a language server:',
+      format_item = function(client)
+        return client.name
+      end,
+    }, on_choice)
+  elseif #clients < 1 then
+    on_choice(nil)
+  else
+    on_choice(clients[1])
+  end
 end
 
 -- https://stackoverflow.com/a/44058905
 local cached_select_client = (function()
     local cache={}
-    return function(method)
+    return function(method, on_choice)
         local buffer_name=vim.api.nvim_get_current_buf()
         local res=cache[buffer_name]
         if not res then
-            res = select_client(method)
+            res = select_client(method, on_choice)
             cache[buffer_name]=res
         end
         return res
@@ -41,12 +40,33 @@ local cached_select_client = (function()
 end)()
 
 function Cached_formatter(options)
-    local client = cached_select_client("textDocument/formatting")
-    if client == nil then return end
+  local params = vim.lsp.util.make_formatting_params(options)
+  local bufnr = vim.api.nvim_get_current_buf()
+  cached_select_client('textDocument/formatting', function(client)
+    if client == nil then
+      return
+    end
 
-    local params = vim.lsp.util.make_formatting_params(options)
-    return client.request("textDocument/formatting", params, nil, vim.api.nvim_get_current_buf())
+    return client.request('textDocument/formatting', params, nil, bufnr)
+  end)
 
+end
+
+function Cached_formatting_sync(options, timeout_ms)
+  local params = vim.lsp.util.make_formatting_params(options)
+  local bufnr = vim.api.nvim_get_current_buf()
+  cached_select_client('textDocument/formatting', function(client)
+    if client == nil then
+      return
+    end
+
+    local result, err = client.request_sync('textDocument/formatting', params, timeout_ms, bufnr)
+    if result and result.result then
+      vim.lsp.util.apply_text_edits(result.result, bufnr)
+    elseif err then
+      vim.notify('vim.lsp.buf.formatting_sync: ' .. err, vim.log.levels.WARN)
+    end
+  end)
 end
 
 function NoFormat()
@@ -63,7 +83,7 @@ lvim.format_on_save = false
 vim.cmd([[
     augroup cached_autoformat
     autocmd!
-    autocmd BufWritePre * :lua Cached_formatter()
+    autocmd BufWritePre * :lua Cached_formatting_sync()
     augroup END
 ]])
 
